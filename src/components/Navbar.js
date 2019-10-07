3928857fd9f73727c9218101d7a44d5f8485db1b
alejandrogomez <alegomez1@me.com>
2019-10-06 22:00:50 -0400

    Added more components

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render () {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <a href="#" className="navbar-brand">Pawnder</a>
+                </nav>
+            </div>
+        )
+    }
+}
+export default Navbar
